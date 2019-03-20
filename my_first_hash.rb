7d02d1c875c0092f90863669fd897a792063d1fc
Matthew Eric <mf9646a@alumni.american.edu>
2019-03-20 19:38:56 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,26 +1,22 @@
 def my_hash
-
 cities_by_state = {
   "Las Vegas" => "Nevada",
   "Washington" => "District of Columbia",
   "Albuquerque" => "New Mexico",
   "Philadelphia" => "Pennsylvania"
 }
-  
 end
 
 
 def shipping_manifest
  the_manifest = {
-   "whale bone corsets" => 5
-   "porcelain vases" => 2
-   "oil paintings" => 3
+   "whale bone corsets" => '5'
+   "porcelain vases" => '2'
+   "oil paintings" => '3'
    
- }
+
   # fill that hash with key/value pairs that describe the following information: 
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
-
-
 end
 
 def retrieval
