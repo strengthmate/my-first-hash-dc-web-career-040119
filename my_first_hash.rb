3bf268422e9bb01dda6aa271b98d30884d555510
Matthew Eric <mf9646a@alumni.american.edu>
2019-03-20 19:36:17 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,9 +1,9 @@
 def my_hash
 
 cities_by_state = {
-  "Las Vegas" => "Nevada"
-  "Washington" => "District of Columbia"
-  "Albuquerque" => "New Mexico"
+  "Las Vegas" => "Nevada",
+  "Washington" => "District of Columbia",
+  "Albuquerque" => "New Mexico",
   "Philadelphia" => "Pennsylvania"
 }
   
